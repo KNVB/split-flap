6f9eeb83038c9a18df132f41c00252ff52772a00
Roy Tsang <cstsang@hko.gov.hk>
2022-05-12 18:00:52 +0800

    2022-05-12 18:00

--- src/components/splitFlap/Slot.js
+++ src/components/splitFlap/Slot.js
@@ -1,107 +1,93 @@
 import "./Slot.css";
-import { useEffect,useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 let reducer = (state, action) => {
     let result = { ...state };
     let temp;
     switch (action.type) {
         case "backward":
-            result.oldValue = action.oldValue;
-            result.newValue = action.newValue;
-            temp = result.thirdDivClass + " transform0to_90";
-            result.thirdDivClass = temp;
-            temp = result.secondDivClass + " rotate0to90";
-            result.secondDivClass = temp;
             break;
         case 'backward2':
-            temp = result.thirdDivClass.replace("zIndex2", "zIndex4");
-            temp += " rotate_90to0";
-            result.thirdDivClass = temp;
-            temp = result.fourthDivClass.replace("zIndex4", "zIndex2");
-            result.fourthDivClass = temp;
             break;
         case "forward":
-            result.oldValue = action.oldValue;
-            result.newValue = action.newValue;
-            temp = result.firstDivClass + " transform0to90";
-            result.firstDivClass = temp
-            temp = result.fourthDivClass + " rotate0to_90";
-            result.fourthDivClass = temp;
             break;
         case 'forward2':
-            temp = result.firstDivClass.replace("zIndex2", "zIndex4");
-            temp += " rotate90to0";
-            result.firstDivClass = temp;
+
             break;
         case "resume":
-            result.oldValue = result.newValue;
-            result.fourthDivClass = 'halfCard zIndex4';
-            result.firstDivClass = result.fullCard + ' zIndex2';
-            result.secondDivClass = result.fullCard + ' zIndex2';
-            result.thirdDivClass = 'halfCard zIndex2';
+
             break;
         default:
             break;
     }
     return result;
 };
-export default function Slot({action,className,hinge,newValue,oldValue}){
-    let fullCard = "";
+export default function Slot({ action, className, hinge, newValue, oldValue }) {
+    let fullCard = "",lowerHalfCard="",upperHalfCard="";
     if (hinge === undefined) {
         fullCard = "fullCard-after";
+        lowerHalfCard="lowerHalfCard-after";
+        upperHalfCard="upperHalfCard-after";
     } else {
         if (hinge === true) {
             fullCard = "fullCard-after";
+            lowerHalfCard="lowerHalfCard-after";
+            upperHalfCard="upperHalfCard-after";
         } else {
             fullCard = "fullCard";
+            lowerHalfCard="lowerHalfCard";
+            upperHalfCard="upperHalfCard";
         }
     }
     const [objList, updateObjList] = useReducer(reducer, {
         oldValue: oldValue,
         newValue: newValue,
         action: "forward" || action,
-        firstDivClass: fullCard + ' zIndex2',
-        secondDivClass: fullCard + ' zIndex2',
-        thirdDivClass: 'halfCard zIndex2',
-        fourthDivClass: 'halfCard zIndex4',
-        "fullCard": fullCard
+        lowerDivClass:lowerHalfCard+" zIndex2",
+        middleDivClass:"hide",
+        upperDivClass:upperHalfCard+" zIndex4",
+        "fullCard": fullCard,
+        "lowerHalftCard": lowerHalfCard,
+        "upperHalfCard":upperHalfCard
     });
     useEffect(() => {
         updateObjList({ type: action, oldValue, newValue });
     }, [action, oldValue, newValue]);
-    let firstDivHandle = () => {
-        updateObjList({ type: 'resume' });
+
+    let upperHandler = () => {
+        
     }
-    let secondDivHandle = () => {
-        updateObjList({ type: 'backward2' });
+    let middleHandler = () => {
+        
     }
-    let thirdDivHandle = () => {
-        updateObjList({ type: 'resume' });
-    }
-    let fourthDivHandle = () => {
-        updateObjList({ type: 'forward2' });
+    let lowerHandler = () => {
+        
     }
     return (
-        <div className={className} style={{"position": "relative",
-                                            "boxSizing": "border-box",
-                                            "transformStyle":"preserve-3d"}}>
+        <div className={className} style={{
+            "position": "relative",
+            "boxSizing": "border-box",
+            "transformStyle": "preserve-3d"
+        }}>
             <div
-                className={objList.firstDivClass}
-                onAnimationEndCapture={firstDivHandle}>
+                id="base"
+                className="fullCard-after zIndex2">
                 {objList.newValue}
             </div>
             <div
-                className={objList.secondDivClass}                
-                onAnimationEndCapture={secondDivHandle}>
+                className={objList.upperDivClass}
+                id="upper"
+                onAnimationEndCapture={upperHandler}>
                 {objList.oldValue}
             </div>
             <div
-                className={objList.thirdDivClass}                
-                onAnimationEndCapture={thirdDivHandle}>
-                {objList.newValue}
+                id="middle"
+                className={objList.middleDivClass}
+                onAnimationEndCapture={middleHandler}>
             </div>
             <div
-                className={objList.fourthDivClass}                
-                onAnimationEndCapture={fourthDivHandle}>
+                className={objList.lowerDivClass}
+                id="lower"
+                onAnimationEndCapture={lowerHandler}>
                 {objList.oldValue}
             </div>
         </div>
