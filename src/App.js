0b5e15b18276df9e1a6dcef51bce18f67874e184
Roy Tsang <cstsang@hko.gov.hk>
2022-08-26 15:02:40 +0800

    Complete the QQ.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./SplitFlap.css";
 import QQ from "./components/qq/QQ";
 import SplitFlap from './components/splitFlap/SplitFlap';
 export default function App() {
-  let wordList = [<img alt="" src="img/1_100.png" />, <img alt="" src="img/2_100.png" />, <img alt="" src="img/3_100.png" />, <img alt="" src="img/4_100.png" />];
+  let wordList = [
+    <img alt="" src="img/0_100.png" />, <img alt="" src="img/1_100.png" />, 
+    <img alt="" src="img/2_100.png" />, <img alt="" src="img/3_100.png" />,
+    <img alt="" src="img/4_100.png" />, <img alt="" src="img/5_100.png" />, 
+    <img alt="" src="img/6_100.png" />, <img alt="" src="img/7_100.png" />, 
+    <img alt="" src="img/8_100.png" />, <img alt="" src="img/9_100.png" />
+  ];
   //let wordList = ["Good Game","Oh Yes","No"];
   return (
     <div style={{ "margin": "10px" }}>
@@ -17,6 +23,7 @@ export default function App() {
       */}
       {
         <QQ
+          hinge={false}
           timing={1000}
           wordList={wordList}/>
       }
--- src/components/qq/QQ.js
+++ src/components/qq/QQ.js
@@ -3,17 +3,23 @@ import { useEffect, useReducer } from 'react';
 let reducer= (state, action) => {
     return action;
 }
-export default function QQ({ timing, wordList }) {
+export default function QQ({hinge, timing, wordList }) {
     const [itemList, updateItemList] = useReducer(reducer,{action:"backward", newIndex: 1, oldIndex: 0 });
     let trigger = () => {
         //console.log('======================================================');
-        //console.log('Kicked by interval,Has focus:' + document.hasFocus());
+        console.log('Kicked by interval,Has focus:' + document.hasFocus());
 
-        let temp = {action:"backward"};
+        let temp = {};
         temp.oldIndex = itemList.newIndex;
         temp.newIndex = itemList.newIndex + 1;
         if (temp.newIndex === wordList.length) {
             temp.newIndex = 0;
+        }
+        //console.log(temp);
+        if (document.hasFocus()){
+            temp.action="backward";
+        } else {
+            temp.action="init";
         }
         updateItemList(temp);
     }
@@ -25,12 +31,11 @@ export default function QQ({ timing, wordList }) {
         };
     });
     
-    
-    
     return (
         <Slot
             action={itemList.action}
             className="splitFlap"
+            hinge={hinge}
             newValue={wordList[itemList.newIndex]}
             oldValue={wordList[itemList.oldIndex]}
         />
